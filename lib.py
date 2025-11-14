b88fa745e05aaaed2918585f9fff915620aaa500
Pasin <pasin0931@gmail.com>
2025-11-14 16:23:54 +0700

    Completed lib.py file (No bug found yet)

--- lib.py
+++ lib.py
@@ -1,7 +1,7 @@
 import turtle
 import random
 
-class Painter:
+class Brush:
     def __init__(self, num_sides, size, orientation, location, color, border_size):
         self.num_sides = num_sides
         self.size = size
@@ -10,7 +10,7 @@ class Painter:
         self.color = color
         self.border_size = border_size
     
-    def draw_polygon(self) -> None:
+    def draw(self) -> None:
         turtle.penup()
         turtle.goto(self.location[0], self.location[1])
         turtle.setheading(self.orientation)
@@ -21,18 +21,41 @@ class Painter:
             turtle.forward(self.size)
             turtle.left(360/self.num_sides)
         turtle.penup()
-
+        
+    def draw_inside(self):
+        turtle.penup()
+        turtle.goto(self.location[0], self.location[1])
+        turtle.setheading(self.orientation)
+        turtle.color(self.color)
+        turtle.pensize(self.border_size)
+        turtle.pendown()
+        for _ in range(self.num_sides):
+            turtle.forward(self.size)
+            turtle.left(360/self.num_sides)
+        turtle.penup()
+        
+        for i in range(0, 2):
+            reduction_ratio = 0.618
+            turtle.forward(self.size*(1-reduction_ratio)/2)
+            turtle.left(90)
+            turtle.forward(self.size*(1-reduction_ratio)/2)
+            turtle.right(90)
+            self.location[0] = turtle.pos()[0]
+            self.location[1] = turtle.pos()[1]
+            self.size *= reduction_ratio
+            self.draw()
+            
     def setup(self, speed=int, bg=str, tracer=int, colormode=int) -> None:
         turtle.speed(speed)
         turtle.bgcolor(bg)
         turtle.tracer(tracer)
         turtle.colormode(colormode)
         
-class User:
+class Painter:
     def __init__(self):
         return None
     
-    def user_choice(self):
+    def painter_choice(self):
         while True:
             try:
                 in_ = int(input("Which art do you want to generate? Enter a number between 1 to 9 inclusive: "))
@@ -43,13 +66,28 @@ class User:
                 print("Invalid input. Please enter a number between 1-9")
 
     def drawing_prep(self):
-        num_sides = random.randint(3, 5) # triangle, square, or pentagon
-        size = random.randint(50, 150)
-        orientation = random.randint(0, 90)
-        location = [random.randint(-200, 200), random.randint(-200, 200)]
+        num_sides = self.random_shape()
+        size = self.random_size()
+        orientation = self.random_orientation()
+        location = self.random_loc()
         color = self.get_new_color()
-        border_size = random.randint(1, 10)
+        border_size = self.random_border()
         return num_sides, size, orientation, location, color, border_size
+    
+    def random_shape(self):
+        return random.randint(3, 5) # triangle, square, or pentagon
+        
+    def random_size(self):
+        return random.randint(50, 150)
+    
+    def random_orientation(self):
+        return random.randint(0, 90)
+        
+    def random_loc(self):
+        return [random.randint(-200, 200), random.randint(-200, 200)]
     
     def get_new_color(self):
         return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    
+    def random_border(self):
+        return random.randint(1, 10)
--- main.py
+++ main.py
@@ -1,30 +1,16 @@
-from lib import User, Painter
+from lib import Painter, Brush
 import turtle, random
 
-usr = User()
-num_sides, size, orientation, location, color, border_size = usr.drawing_prep()
+painter = Painter()
+num_sides, size, orientation, location, color, border_size = painter.drawing_prep()
 
-painter = Painter(num_sides, size, orientation, location, color, border_size)
-painter.setup(0, "black", 0, 255)
+brush = Brush(num_sides, size, orientation, location, color, border_size)
+brush.setup(0, "black", 0, 255)
 
-painter.draw_polygon()
+# brush.draw_inside()
+# brush.draw()
 
-reduction_ratio = 0.618
-
-# reposition the turtle and get a new location
-turtle.penup()
-turtle.forward(size*(1-reduction_ratio)/2)
-turtle.left(90)
-turtle.forward(size*(1-reduction_ratio)/2)
-turtle.right(90)
-location[0] = turtle.pos()[0]
-location[1] = turtle.pos()[1]
-
-# adjust the size according to the reduction ratio
-size *= reduction_ratio
-
-# draw the second polygon embedded inside the original
-painter.size = size
-painter.draw_polygon()
+# for i in range(random.randint(25, 35)):
+#     draw
 
 turtle.done()
